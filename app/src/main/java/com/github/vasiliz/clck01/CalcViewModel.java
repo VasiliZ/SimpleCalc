fd05f330b2572af63de11925ed8628fd240bbe7a
Z <zaicev.vasili@gmail.com>
2019-01-27 15:37:49 +0300

    Initial commit

---
+++ app/src/main/java/com/github/vasiliz/clck01/CalcViewModel.java
@@ -0,0 +1,198 @@
+package com.github.vasiliz.clck01;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EmptyStackException;
+import java.util.List;
+import java.util.Stack;
+
+public class CalcViewModel extends AndroidViewModel {
+
+    MutableLiveData<String> liveData = new MutableLiveData<>();
+    MutableLiveData<String> tempValues = new MutableLiveData<>();
+    private final ArrayList<String> strings = new ArrayList<>();
+    private final StringBuilder calculateString = new StringBuilder();
+    private final StringBuilder stringBuilder = new StringBuilder();
+    private final String[] operators = {"+", "-", "/", "*"};
+    private final String EMPTY_STRING = "";
+    private final String END_DOUBLE_VALUE = ".0";
+
+    private final Stack<String> stackP = new Stack<>();
+    private final Stack<String> operations = new Stack<>();
+    private final Stack<String> calculationStack = new Stack<>();
+
+    public CalcViewModel(@NonNull final Application application) {
+        super(application);
+    }
+
+    //todo action here
+
+    void setClick(final String action) {
+        calculateString.append(action);
+        stringBuilder.append(action);
+        liveData.setValue(stringBuilder.toString());
+    }
+
+    void doBackspace() {
+        stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+        calculateString.delete(calculateString.length() - 1, calculateString.length());
+        liveData.setValue(stringBuilder.toString());
+
+    }
+
+    void clearBuffer() {
+        stringBuilder.delete(0, stringBuilder.length());
+        calculateString.delete(0, calculateString.length());
+        strings.clear();
+        liveData.setValue(stringBuilder.toString());
+        tempValues.setValue(calculateString.toString());
+    }
+
+    void onAction(final String action) {
+        strings.add(stringBuilder.toString());
+        if (calculateString.toString().endsWith(action)) {
+            return;
+        }
+        calculateString.append(action);
+        tempValues.setValue(calculateString.toString());
+        stringBuilder.delete(0, stringBuilder.length());
+        liveData.setValue(stringBuilder.toString());
+        strings.add(action);
+    }
+
+    public void setDot(final String dot) {
+        if (!stringBuilder.toString().contains(getApplication()
+                .getResources()
+                .getString(R.string.dot))) {
+            stringBuilder.append(dot);
+            calculateString.append(dot);
+            liveData.setValue(stringBuilder.toString());
+        }
+    }
+
+    void calculate() {
+        if (strings == null) {
+            return;
+        }
+        strings.add(stringBuilder.toString());
+        tempValues.setValue(calculateString.toString());
+
+        convertToRPN(strings);
+        calculationStack.clear();
+        Collections.reverse(stackP);
+        try {
+            while (!stackP.empty()) {
+                Log.d("item", stackP.peek());
+                final String token = stackP.pop();
+                if (isNumber(token)) {
+                    calculationStack.push(token);
+                } else if (isOperator(token)) {
+                    final double op1 = Double.parseDouble(calculationStack.pop());
+                    final double op2 = Double.parseDouble(calculationStack.pop());
+                    switch (token) {
+                        case "+":
+                            calculationStack.push(String.valueOf(op2 + op1));
+                            break;
+                        case "-":
+                            calculationStack.push(String.valueOf(op2 - op1));
+                            break;
+                        case "*":
+                            calculationStack.push(String.valueOf(op2 * op1));
+                            break;
+                        case "/":
+                            if (op2 == 0 || op1 == 0) {
+                                liveData.setValue(getApplication()
+                                        .getResources()
+                                        .getString(R.string.error));
+                                strings.clear();
+
+                                break;
+                            }
+                            final String total = String.valueOf(op2 / op1);
+                            calculationStack.push(total);
+                            break;
+                    }
+                }
+            }
+            final String result = calculationStack.pop();
+            final String cutString;
+            if (result.endsWith(END_DOUBLE_VALUE)) {
+                cutString = result.substring(0, result.lastIndexOf(getApplication()
+                        .getResources()
+                        .getString(R.string.dot)));
+                liveData.setValue(cutString);
+            }else {
+                liveData.setValue(result);
+            }
+
+            strings.clear();
+            tempValues.setValue(EMPTY_STRING);
+
+        } catch (final EmptyStackException e) {
+            liveData.setValue(getApplication().getResources().getString(R.string.error));
+        }
+
+    }
+
+    private void convertToRPN(final List<String> strings) {
+        for (int i = 0; i < strings.size(); i++) {
+            if (isNumber(strings.get(i))) {
+                stackP.push(strings.get(i));
+            } else if (isOperator(strings.get(i))) {
+                while (!operations.empty()
+                        && isOperator(operations.lastElement())
+                        && priority(strings.get(i))
+                        <= priority(operations.lastElement())) {
+                    stackP.push(operations.pop());
+                }
+                operations.push(strings.get(i));
+            }
+        }
+        while (!operations.empty()) {
+            stackP.push(operations.pop());
+        }
+    }
+
+    private boolean isNumber(final String token) {
+        try {
+            Double.parseDouble(token);
+        } catch (final Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isOperator(final String token) {
+        for (final String op : operators) {
+            if (op.equals(token)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int priority(final String operation) {
+        if (operation
+                .equals(getApplication()
+                        .getResources()
+                        .getString(R.string.minus))
+                || operation
+                .equals(getApplication()
+                        .getResources()
+                        .getString(R.string.plus))) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/github/vasiliz/clck01/MainActivity.java
@@ -0,0 +1,185 @@
+package com.github.vasiliz.clck01;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView viewCalcActions, tempValue;
+    private CalcViewModel calcViewModel;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewCalcActions = findViewById(R.id.view_input_textview);
+        init();
+    }
+
+    private void init() {
+
+        tempValue = findViewById(R.id.temp_textView);
+        findViewById(R.id.input_one_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.one));
+            }
+        });
+        findViewById(R.id.input_two_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.two));
+            }
+        });
+        findViewById(R.id.input_three_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.three));
+            }
+        });
+        findViewById(R.id.input_four_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.four));
+            }
+        });
+        findViewById(R.id.input_five_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.five));
+            }
+        });
+        findViewById(R.id.input_six_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.six));
+            }
+        });
+        findViewById(R.id.input_seven_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.seven));
+            }
+        });
+        findViewById(R.id.input_eight_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.eight));
+            }
+        });
+        findViewById(R.id.input_nine_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.nine));
+            }
+        });
+        findViewById(R.id.input_zero_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setClick(getResources().getString(R.string.zero));
+            }
+        });
+
+        findViewById(R.id.plus_minus_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                calcViewModel.setClick("-");
+            }
+        });
+
+        //action
+
+        findViewById(R.id.clear_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.clearBuffer();
+            }
+        });
+        findViewById(R.id.backspace_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.doBackspace();
+            }
+        });
+
+        findViewById(R.id.divide_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.onAction(getResources().getString(R.string.divide));
+            }
+        });
+        findViewById(R.id.multiply_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.onAction(getResources().getString(R.string.multiply));
+            }
+        });
+        findViewById(R.id.minus_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.onAction(getResources().getString(R.string.minus));
+            }
+        });
+        findViewById(R.id.plus_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.onAction(getResources().getString(R.string.plus));
+            }
+        });
+        findViewById(R.id.equalsign_button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.calculate();
+            }
+        });
+        findViewById(R.id.input_button_dot).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                calcViewModel.setDot(getResources().getString(R.string.dot));
+            }
+        });
+
+        calcViewModel = ViewModelProviders.of(this).get(CalcViewModel.class);
+        calcViewModel.tempValues.observe(this, new Observer<String>() {
+
+            @Override
+            public void onChanged(@Nullable final String s) {
+                tempValue.setText(s);
+            }
+        });
+        calcViewModel.liveData.observe(this, new Observer<String>() {
+
+            @Override
+            public void onChanged(@Nullable final String s) {
+                viewCalcActions.setText(s);
+            }
+        });
+
+    }
+}
